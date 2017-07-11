3b9288d81d86a3c7cd69e429ca9086bf55dc25d0
TuanLe <tuan.le@exe.com.vn>
2017-07-11 15:52:14 +0700

    Remove request

--- common.ts
+++ common.ts
@@ -2,4 +2,3 @@ export * from './source/common/Mailer';
 export * from './source/common/PDFHelper';
 export * from './source/common/RabbitMQ';
 export * from './source/common/PasswordEncoder';
-export * from './source/common/Request';
